79909eaed19677e315c1aeb6b9ae64a95d22fc91
Michel França <fr.mich17@gmail.com>
2026-01-14 05:46:32 -0300

    test(student): add unit test for student response mapper

---
+++ api/src/modules/student/infra/mappers/student-reponse.mapper.spec.ts
@@ -0,0 +1,28 @@
+import { Cpf } from '@modules/student/domain/value-objects/cpf.vo';
+import { Email } from '@modules/student/domain/value-objects/email.vo';
+import { Name } from '@modules/student/domain/value-objects/name.vo';
+import { Ra } from '@modules/student/domain/value-objects/ra.vo';
+import { Student } from '../../domain/entities/student';
+import { StudentResponseMapper } from './student-response.mapper';
+
+describe('StudentResponseMapper', () => {
+	it('should map Student domain to StudentResponseDto correctly', () => {
+		const student = new Student(
+			'c3a759a3-92fa-484d-8b7d-0a5bd83fdb9c',
+			Name.create('John Doe'),
+			Email.create('JOHN.DOE@EXAMPLE.COM'),
+			Cpf.create('123.456.789-01'),
+			Ra.create('a1b2c3'),
+		);
+
+		const dto = StudentResponseMapper.toDto(student);
+
+		expect(dto).toEqual({
+			id: 'c3a759a3-92fa-484d-8b7d-0a5bd83fdb9c',
+			name: 'John Doe',
+			email: 'john.doe@exemple.com',
+			cpf: '12345678901',
+			ra: 'A1B2C3',
+		});
+	});
+});
