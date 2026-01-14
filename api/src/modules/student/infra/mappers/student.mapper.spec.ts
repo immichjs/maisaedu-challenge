678b72d609a69d8cd1fad7ae276db329e588caa2
Michel França <fr.mich17@gmail.com>
2026-01-14 03:27:34 -0300

    test(student): add unit tests for student mapper

---
+++ api/src/modules/student/infra/mappers/student.mapper.spec.ts
@@ -0,0 +1,47 @@
+import { StudentEntity } from '../entities/student.entity';
+import { StudentMapper } from './student.mapper';
+
+import { Student } from '../../domain/entities/student';
+import { Cpf } from '../../domain/value-objects/cpf.vo';
+import { Email } from '../../domain/value-objects/email.vo';
+import { Name } from '../../domain/value-objects/name.vo';
+import { Ra } from '../../domain/value-objects/ra.vo';
+
+describe('StudentMapper', () => {
+	it('should map entity to domain correctly', () => {
+		const entity = new StudentEntity();
+		entity.id = 'c3a759a3-92fa-484d-8b7d-0a5bd83fdb9c';
+		entity.name = 'John Doe';
+		entity.email = 'john.doe@example.com';
+		entity.cpf = '12345678901';
+		entity.ra = 'A1B2C3';
+
+		const student = StudentMapper.toDomain(entity);
+
+		expect(student).toBeInstanceOf(Student);
+		expect(student.id).toBe('c3a759a3-92fa-484d-8b7d-0a5bd83fdb9c');
+		expect(student.name).toBe('John Doe');
+		expect(student.email).toBe('john.doe@example.com');
+		expect(student.cpf).toBe('12345678901');
+		expect(student.ra).toBe('A1B2C3');
+	});
+
+	it('should map domain to entity correctly', () => {
+		const student = new Student(
+			'c3a759a3-92fa-484d-8b7d-0a5bd83fdb9c',
+			Name.create('John Doe'),
+			Email.create('JOHN.DOE@EXAMPLE.COM'),
+			Cpf.create('123.456.789-01'),
+			Ra.create('a1b2c3'),
+		);
+
+		const entity = StudentMapper.toEntity(student);
+
+		expect(entity).toBeInstanceOf(StudentEntity);
+		expect(entity.id).toBe('c3a759a3-92fa-484d-8b7d-0a5bd83fdb9c');
+		expect(entity.name).toBe('John Doe');
+		expect(entity.email).toBe('john.doe@example.com');
+		expect(entity.cpf).toBe('12345678901');
+		expect(entity.ra).toBe('A1B2C3');
+	});
+});
